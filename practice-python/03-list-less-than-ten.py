0a6315a35f5cbb38c2df102633aeb44a886e45e9
Mark de Villiers <markdeVilliers@gmail.com>
2015-11-17 02:15:26 -0800

    correct list less than 10

--- practice-python/03-list-less-than-ten.py
+++ practice-python/03-list-less-than-ten.py
@@ -1,9 +1,9 @@
 
-# write a program that prints out all the elements of the list that are less than 5.
+# write a program that prints out all the elements of the list that are less than 10.
 def main():
     
 	a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-	predicated = [x for x in a if x < 5]
+	predicated = [x for x in a if x < 10]
 	print (predicated)
 
 
