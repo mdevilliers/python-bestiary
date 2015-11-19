1c1ed81da12f691a386f5e344cab28648a0ef670
Mark de Villiers <markdeVilliers@gmail.com>
2015-11-19 08:08:08 -0800

    added solution to nesting

---
+++ codility/nesting.py
@@ -0,0 +1,30 @@
+# https://codility.com/programmers/task/nesting
+
+def main():
+    print(solution("(()(())())")) # 1
+    print(solution("())")) # 0
+
+def solution(A):
+    
+    stack = []
+    
+    for c in A:
+        
+        if len(stack) == 0 :
+            stack.append(c)
+        else :
+            peek = stack.pop()
+            
+            if peek == "(" and c == ")" :
+                continue
+            else :
+                stack.append(peek)
+                stack.append(c)
+     
+    if len(stack) == 0 :
+        return 1
+     
+    return 0
+
+if __name__ == "__main__":
+    main()
