df69a50ff608bc1d90e1e7606c57c5c553b34ba7
ubuntu <markdevilliers@gmail.com>
2015-12-09 13:34:41 -0800

    completed part1 and part2 of day 6

--- adventofcode/06.py
+++ adventofcode/06.py
@@ -5,63 +5,31 @@ def main():
     # print(parse("turn on 489,959 through 759,964"))
     # print(parse("turn off 489,959 through 759,964"))
     # print(parse("toggle 489,959 through 759,964"))
-    
-    # print(board)
-    # # toggle(board,0,0,10,10) # all on
-    # # print(board)
-    board = create_board(10,10)
-    toggle(board,0,0,5,5) # first quadrent
-    debug_board(board,10,10)
-    print(count_lights_on(board)) # 25
-    on(board,5,5,10,10) # last quadrent
-    debug_board(board,10,10)
-    print(count_lights_on(board)) # 50
-    off(board, 4,4,6,6) # turn of four midddle
-    debug_board(board,10,10)
-    print(count_lights_on(board)) # 48
-    off(board,0,0,10,0) # swith off first row
-    debug_board(board,10,10)
-    # print(iterateFile("06_data.txt"))
 
-def debug_board(board, length=1000, height=1000) :
-    for x in xrange(0,length):
-        for y in xrange(0,height):
-          key = get_key(x,y)         
-          print(board[key]),
-        print("")
+    # board = create_board(10,10)
+    # toggle(board,0,0,5,5) # first quadrent
+    # debug_board(board)
+    # print(count_lights_on(board)) 
+    # on(board,5,5,9,9) # last quadrent
+    # debug_board(board)
+    # print(count_lights_on(board)) 
+    # off(board, 4,4,5,5) # turn of four midddle
+    # debug_board(board)
+    # print(count_lights_on(board)) 
+    # off(board,0,0,9,0) # swith off first row
+    # debug_board(board)
+    # print(count_lights_on(board)) 
+    print(iterateFile_part1("06_data.txt")) # 569999
+    print(iterateFile_part2("06_data.txt")) # 17836115
 
-def get_key(x,y) :
-    return "{0}{1}".format(x,y)
+def do(board, startx, starty, endx, endy, func) :
+    length = len(board[0])
+    for x in range(startx, endx + 1):
+        for y in range(starty, endy + 1):
+            board[x][y] = func(board[x][y])
 
-def toggle(board, startx, starty, endx, endy) :    
-    points = [ (x,y) for x in xrange(startx,endx) for y in xrange(starty,endy)]
-    for x in xrange(0, len(points)):
-        key = get_key(points[x][0],points[x][1])
-        board[key] = 0 if board[key] == 1 else 1
-
-def on(board, startx, starty, endx, endy) :    
-    points = [ (x,y) for x in xrange(startx,endx) for y in xrange(starty,endy)]
-    set_positions(board,points,1)
-
-def off(board, startx, starty, endx, endy) :    
-    points = [ (x,y) for x in xrange(startx,endx) for y in xrange(starty,endy)]
-    set_positions(board,points,0)
-
-def set_positions(board, points, value) :
-    for x in xrange(0, len(points)):
-        key = get_key(points[x][0],points[x][1])
-        board[key] = value
-
-def create_board(length = 1000, height = 1000) :
-
-    arr = {}
-
-    for x in xrange(0,length):
-        for y in xrange(0,height):
-          key = get_key(x,y)         
-          arr[key] = 0
-
-    return arr
+def create_board(length = 1000, height = 1000, value=False) :
+    return [[value] * length for n in range(height)]
 
 def parse(command) :
 
@@ -83,26 +51,62 @@ def parse(command) :
 def parse_coords(coords_str):
     return[int(x) for x in coords_str.split(",")]
 
-def iterateFile(filename):
+def iterateFile_part1(filename):
     
-    board = create_board(999,999)
+    board = create_board()
 
     with open(filename) as f:
         for line in f:
             command, start, end = parse(line.strip())
             print(command, start, end)
             if command == "on" :
-                on(board, start[0], start[1], end[0], end[1])
+                do(board, start[0], start[1], end[0], end[1],lambda x: True )
             if command == "off" :
-                off(board, start[0], start[1], end[0], end[1])
+                do(board, start[0], start[1], end[0], end[1],lambda x: False)
             if command == "toggle" :
-                toggle(board, start[0], start[1], end[0], end[1])
+                do(board, start[0], start[1], end[0], end[1],lambda x: False if x else True )
 
     return count_lights_on(board)
 
+def iterateFile_part2(filename):
+    
+    board = create_board(value=0)
+
+    with open(filename) as f:
+        for line in f:
+            command, start, end = parse(line.strip())
+            print(command, start, end)
+            if command == "on" :
+                do(board, start[0], start[1], end[0], end[1],lambda x: x + 1 )
+            if command == "off" :
+                do(board, start[0], start[1], end[0], end[1],lambda x: max(x - 1, 0))
+            if command == "toggle" :
+                do(board, start[0], start[1], end[0], end[1],lambda x: x + 2 )
+
+    return count_brightness(board)
+
 def count_lights_on(board) :
-    predicated = { k: v for k, v in board.iteritems() if v == 1 }
-    return len(predicated)  
+    count = 0
+    length = len(board[0])
+    for x in xrange(length):
+        for y in xrange(length):      
+          count += 1 if board[x][y] == True else 0
+    return count
+
+def count_brightness(board) :
+    count = 0
+    length = len(board[0])
+    for x in xrange(length):
+        for y in xrange(length):      
+          count += board[x][y]
+    return count
+
+def debug_board(board) :
+    length = len(board[0])
+    for x in xrange(0,length):
+        for y in xrange(0,length):      
+          print(1 if board[x][y] == True else 0),
+        print("")
 
 if __name__ == "__main__":
     main()
